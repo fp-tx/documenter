0202bb13f5e48dd952e39e0da631be7c3ff53c43
Jacob Alford <jacob.alford@me.com>
2024-03-18 16:25:15 -0600

    chore: initialize repository

--- tsup.config.js
+++ tsup.config.js
@@ -1,3 +1,27 @@
 import { makeConfig } from '@fp-tx/build-tools'
 
-export default makeConfig({})
+export default makeConfig(
+  {
+    buildType: 'dual',
+    buildMode: {
+      type: 'Multi',
+      entrypointGlobs: [
+        'src/Api.ts',
+        'src/APITraversal.ts',
+        'src/Documenter.ts',
+        'src/FileService.ts',
+        'src/Markdown.ts',
+        'src/MarkdownFiles.ts',
+        'src/Prettier.ts',
+        'src/bin.ts',
+        'src/index.ts',
+      ],
+    },
+  },
+  {
+    clean: true,
+    target: 'node18',
+    splitting: true,
+    external: ['prettier'],
+  },
+)
